4361531b926ce1fddb80ea9b9f86a9666fbf2fb2
Pierce <Pierce@PIERCE>
2024-04-11 11:59:06 -0400

    use json and js to load works for website

---
+++ app/app.js
@@ -0,0 +1,50 @@
+// Variables
+const webSection = document.getElementsByClassName("websiteSection");
+let porfolioData = []; // Array to hold Json data
+
+// Get Data from Json
+function getData() {
+  $.get("data/worksData.json", function (data) {
+    // Assign Fetch Data to global Variable
+    porfolioData = data;
+    console.log(data);
+  }).fail(function (error) {
+    console.log("Error: ", error);
+  });
+}
+
+// Load Porfolio Works
+function loadPorfolio() {
+  $(".websiteSection").html("");
+  $.each(porfolioData.Works, function (index, work) {
+    console.log(index);
+    $(".websiteSection").append(`<div class="websitePrevBox">
+    <div class="websitePrevPic ${work.WebsiteImage}Pic">
+      <div class="aboutSite-Text">
+        <h2>${work.WebsiteTitle}</h2>
+        <p>${work.WebsiteCodeLangs}</p>
+        <button class="viewSite">
+          <a
+            href="${work.WebsiteURL}"
+            target="_blank"
+          >
+            View Site
+          </a>
+        </button>
+      </div>
+    </div>
+  </div>`);
+  });
+}
+
+function initListeners() {}
+
+$(document).ready(function () {
+  getData();
+  initListeners();
+  //   loadPorfolio();
+});
+// Load Porfolio data when after document is ready
+$(window).on("load", function () {
+  loadPorfolio();
+});
